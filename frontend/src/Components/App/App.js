dd89844a26e6ada4ebbec85855de7873340de099
vinciusb <65973642+vinciusb@users.noreply.github.com>
2023-02-03 14:38:28 -0300

    Frontend setup

---
+++ frontend/src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import styled, { keyframes } from 'styled-components';
+
+// const Styled = {};
+
+function App() {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default App;
